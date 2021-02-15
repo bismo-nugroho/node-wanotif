cd850f7f937eb991612d7a8bf050e55dd2a950be
bismo-nugroho <bismo.nugroho@gmail.com>
2021-02-15 13:27:17 +0700

    commit

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ function checkElapsed() {
 
 function getAck(stats) {
 	//ack map
-
 	switch (stats) {
 		case 'SERVER_ACK':
 			return 1;
@@ -314,7 +313,7 @@ async function starts() {
 			const isNsfw = isGroup ? nsfw.includes(from) : false
 			const isSimi = isGroup ? samih.includes(from) : false
 			const isOwner = ownerNumber.includes(sender);
-			
+
 			const isUrl = (url) => {
 				return url.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/, 'gi'))
 			}
